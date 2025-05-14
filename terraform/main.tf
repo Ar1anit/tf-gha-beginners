ca00550782bee807a62e5fbb4567b80d254d19ee
Ari <t-asheholli@microsoft.com>
2025-05-14 10:51:32 +0200

    Update resource group and virtual network locations to eastus

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,14 +18,14 @@ data "azurerm_client_config" "current" {}
 #Create Resource Group
 resource "azurerm_resource_group" "example" {
   name     = "tf-gha-rg"
-  location = "germanywest"
+  location = "eastus"
 }
 
 #Create Virtual Network
 resource "azurerm_virtual_network" "vnet" {
   name                = "tf-gha-vnet"
   address_space       = ["192.168.0.0/16"]
-  location            = "germanywest"
+  location            = "eastus"
   resource_group_name = azurerm_resource_group.example.name
 }
 
